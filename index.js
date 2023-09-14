d0d9385439e6302e54a24790bf24b4beab77f946
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-15 00:29:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const buttonLocal = document.getElementById('button-local');
+const buttonWorker = document.getElementById('button-worker');
+
+buttonLocal.addEventListener('click', () => {
+  console.log('buttonLocal clicked');
+});
+
+buttonWorker.addEventListener('click', () => {
+  console.log('buttonWorker clicked');
+});
