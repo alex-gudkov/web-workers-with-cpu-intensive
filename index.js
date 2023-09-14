6162b8c9030def8d9eef12a8a9737ace0aad04ff
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-15 00:52:53 +0300

    add fibonacci function calc

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+import { fibonacci } from './fibonacci.js';
+
+console.log(import.meta);
+
 const buttonLocal = document.getElementById('button-local');
 const buttonWorker = document.getElementById('button-worker');
 
 buttonLocal.addEventListener('click', () => {
-  console.log('buttonLocal clicked');
+  console.log('#button-local clicked');
+
+  const result = fibonacci(42);
+
+  console.log('result', result);
 });
 
 buttonWorker.addEventListener('click', () => {
-  console.log('buttonWorker clicked');
+  console.log('#button-worker clicked');
 });
